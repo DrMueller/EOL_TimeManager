716552518a3ef188f71010f024e21c25c125761a
Matthias Müller <matthias.mueller@trivadis.com>
2019-10-04 11:54:21 +0200

    Init

---
+++ Sources/WpfUI/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Mmu.Mlh.WpfCoreExtensions.Areas.Initialization.Orchestration.Models;
+using Mmu.Mlh.WpfCoreExtensions.Areas.Initialization.Orchestration.Services;
+
+namespace Mmu.TimeManager.WpfUI
+{
+    public partial class App : Application
+    {
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var assembly = typeof(App).Assembly;
+            var appConfig = WpfAppConfig.CreateWithDefaultIcon(assembly, "Time Manager");
+            await AppStartService.StartAppAsync(appConfig);
+        }
+    }
+}
